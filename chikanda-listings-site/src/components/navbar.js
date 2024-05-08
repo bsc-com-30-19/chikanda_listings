63ebcfcf4d3b9a32f3950aafe357bcc0bddae10e
Paul Kabondo <bsc-com-30-19@unima.ac.mw>
2024-05-08 17:30:20 +0200

    Started working on a navbar

---
+++ chikanda-listings-site/src/components/navbar.js
@@ -0,0 +1,9 @@
+function NavBar(){
+    return(
+        <nav ClassName="bg-p_cyan">
+            <div>texts</div>
+        </nav>
+    )
+}
+
+export default NavBar;
--- chikanda-listings-site/tailwind.config.js
+++ chikanda-listings-site/tailwind.config.js
@@ -2,7 +2,11 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      colors:{
+        p_cyan : '#24CBFF'
+      }
+    },
   },
   plugins: [],
 }

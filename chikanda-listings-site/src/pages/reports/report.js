d55462e24de49ab9e11ad623c3f1e5a78461aa66
GiftDevison <bsc-inf-07-22@unima.ac.mw>
2024-05-19 16:05:40 +0200

    Made adjustments. Created a component for reports

--- chikanda-listings-site/src/pages/reports/report.js
+++ chikanda-listings-site/src/pages/reports/report.js
@@ -51,27 +51,7 @@ const Report = () => {
               </table>
             </div>
 
-            <div class="rounded-lg px-4 py-2">
-              <h2 class="mb-2 bg-p_cyan text-left font-bold">Sent Reports and Requests</h2>
-              <table class="border-collapse">
-                <tbody>
-                  <tr>
-                    <td class="px-2 py-1">Title:</td>
-                    <td class="px-2 py-1">A title name here</td>
-                    <th class="px-2 py-1" rowspan="3">Status: Pending</th>
-                  </tr>
-                  <tr>
-                    <td class="px-2 py-1">Type:</td>
-                    <td class="px-2 py-1">Either Request or Report</td>
-                  </tr>
-                  <tr>
-                    <td class="px-2 py-1">Date sent:</td>
-                    <td class="px-2 py-1">xx/xx/xxxx</td>
-                  </tr>
-                </tbody>
-              </table>
             </div>
-          </div>
 
           );
 };

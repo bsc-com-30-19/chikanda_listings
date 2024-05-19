3924b0349c491483c631b45b5dcbf1b0c5c85cf2
GiftDevison <bsc-inf-07-22@unima.ac.mw>
2024-05-19 16:01:10 +0200

    Created the left side of the design based on figma design using a tabble

--- chikanda-listings-site/src/pages/reports/report.js
+++ chikanda-listings-site/src/pages/reports/report.js
@@ -7,7 +7,48 @@ const Report = () => {
     <div class="grid grid-cols-1 gap-4 md:grid-cols-2">
 
       <div class="rounded-lg px-4 py-2">
-        
+        <table class="border-collapse">
+          <h2 class="mb-2 bg-blue-300 text-left font-bold">Make Request or Report</h2>
+          <tbody>
+            <tr>
+              <td class="px-2 py-1">
+                <input type="radio" id="option_a" name="my_radio_group" value="Request"/>
+                  <label for="option_a">Request</label>
+                  <br/>
+                    <input type="radio" id="option_b" name="my_radio_group" value="Report"/>
+                      <label for="option_b">Report</label>
+                    </td>
+                  </tr>
+
+                  <tr>
+                    <td class="px-2 py-1">Title:
+                    </td>
+                    <tr>
+
+                      <tr>
+                        <td class="px-2 py-1">
+                          <input type="text" placeholder="Title here:"/>
+                        </td>
+                        <tr>
+
+
+                          <td class="px-2 py-1">
+
+                          </td>
+                        </tr>
+                        <td class="px-2 py-1">Description:
+
+                        </td>
+                      </tr>
+
+                    </tr>
+                    <td class="px-2 py-1">
+                      <input type="text" placeholder="Provide a description" />
+                    </td>
+                  </tr>
+
+                </tbody>
+              </table>
             </div>
 
             <div class="rounded-lg px-4 py-2">

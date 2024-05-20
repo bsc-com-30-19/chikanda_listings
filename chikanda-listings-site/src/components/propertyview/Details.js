640b3f94b8619df63a3e70a03bb354d23f10fc72
Paul Kabondo <bsc-com-30-19@unima.ac.mw>
2024-05-20 15:03:10 +0200

    Somewhat done making the mainbar of the propertyview

--- chikanda-listings-site/src/components/propertyview/Details.js
+++ chikanda-listings-site/src/components/propertyview/Details.js
@@ -4,18 +4,18 @@ const Details = () =>{
     <div className="rounded-[25px] bg-p_colu my-[36px]">
         <div className="px-[36px] py-[30px]">
             <h1 className="font-bold text-[32px]">Property Details:</h1>
-            <div className="pt-[25px] text-[28px] leading-[50px]">
-                <h2>Rent Per Month:</h2>
+            <div className="pt-[25px] text-[22px] leading-[45px]">
+                <h2 className="text-[25px] font-semibold">Rent Per Month:</h2>
                 <p>XXXXXX MK</p>
-                <h2>Allowed Sex</h2>
+                <h2 className="text-[25px] font-semibold">Allowed Sex</h2>
                 <div className=" inline-flex">
                     <div>
                     <label htmlFor="male">Male</label>
-                    <input type="checkbox" name="male"></input>
+                    <input type="checkbox" value="checked" name="male" disabled checked></input>
                     </div>
                     <div>
                     <label htmlFor="female">Female</label>
-                    <input type="checkbox" name="female"></input>
+                    <input type="checkbox" name="female" disabled></input>
                     </div>
                 </div>
             </div>

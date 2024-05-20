c66e681f90a3d769c96804171ac7ae84a8b9e867
Takondwamulenga <bsc-com-26-22@unima.ac.mw>
2024-05-20 15:14:16 +0200

    made a few changes

--- chikanda-listings-site/src/pages/Notification System.js
+++ chikanda-listings-site/src/pages/Notification System.js
@@ -1,7 +1,16 @@
 import Notification from "../components/NotificationSystem/Notification";
 const NotificationSystem = () => {
-    return(<div>
+    return(<div className = "W-full">
+        <div className = " " >
+        <div className = "h-[200 px] w-[600px] overflow-y-scroll- mx-auto">
         <Notification/>
+        <Notification/>
+        <Notification/>
+        <Notification/>
+        <Notification/> 
+         </div>  
+        </div>
+       
         NotificationSystem
         </div>)
 }

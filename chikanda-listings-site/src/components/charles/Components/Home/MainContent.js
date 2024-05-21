530748f61ca8d54c8b45dced2052202b25e8dec2
Chirles Sinoya <bsc-inf-25-22@unima.ac.mw>
2024-05-21 16:16:23 +0200

    Added property page select page and edited property edit page

---
+++ chikanda-listings-site/src/components/charles/Components/Home/MainContent.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const MainContent = () => {
+  return (
+    <div>
+         <div className="flex flex-col w-9/12 bg-blue-900 h-full">
+        Main content
+        <form></form>
+        </div>
+    </div>
+  )
+}
+
+export default MainContent

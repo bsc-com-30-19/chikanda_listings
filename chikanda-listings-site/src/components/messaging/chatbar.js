2f073e743b4e797ec11077aba6876b30080fe3ce
Paul Kabondo <bsc-com-30-19@unima.ac.mw>
2024-05-23 14:12:32 +0200

    Send text functionality included

--- chikanda-listings-site/src/components/messaging/chatbar.js
+++ chikanda-listings-site/src/components/messaging/chatbar.js
@@ -1,16 +1,32 @@
 import image from "../../assets/img/girl.png";
 import InputMessage from "./inputmessage";
 import Messages from "./messages";
+import { useState } from "react";
+
+
+const USERMESSAGEDATA =[
+    {id: "0", text: "Yes Hello"},
+    {id: "1", text: "I understand"}
+]
+
+let ArrLen = USERMESSAGEDATA.length 
 
 const ChatBar = () =>{
+    const [texts, setTexts] = useState(USERMESSAGEDATA);
+
+    const addUmessage = (m) =>{
+        const newMessage = {id:ArrLen++, text: m}
+        setTexts([...texts, newMessage])
+    }
+
     return (
         <div className="flex-[2.5] bg-[#dbd8db]">
             <div className="flex h-[70px] items-center gap-[10px] bg-[#0E6BA8] px-[25px]">
                 <img src={image} alt='profile pic' className=" h-[45px] w-[45px] rounded-full object-cover"/>
                 <span className=" text-xl font-bold text-p_lave">Jane</span>
             </div>
-            <Messages />
-            <InputMessage />
+            <Messages texts={texts}/>
+            <InputMessage addUmessage={addUmessage}/>
         </div>
     )
 }
--- chikanda-listings-site/src/components/messaging/messages.js
+++ chikanda-listings-site/src/components/messaging/messages.js
@@ -1,13 +1,18 @@
 import Message from "./message";
 import UserMessage from "./usermessage";
 
-const Messages = () =>{
+
+const Messages = (props) =>{
+    
+    const textlist = props.texts?.map((utext) => (<UserMessage id={utext.id} text={utext.text} key={utext.id}/>))
     return (
-        <div className="p-[10px] h-[calc(100%-150px)] overflow-y-scroll">
-            <Message />
-            <Message />
-            <Message />
-            <UserMessage />
+        <div className="p-[10px] h-[calc(100%-150px)] overflow-y-scroll w-full">
+            <ul className="flex flex-col">
+            <Message text = "Hello"/>
+            <Message text = "I have a question"/>
+            <Message text = "Do the self contained rooms have kitchens?"/>
+            {textlist}
+            </ul>
         </div>
     )
 }
